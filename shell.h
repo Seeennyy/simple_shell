e19563c60cca4e4048b352b753d8dee6afe7728a
Seeennyy <seeennnyyy@gmail.com>
2023-07-26 12:39:36 -0700

    done

--- shell.h
+++ shell.h
@@ -215,7 +215,7 @@ list_t *add_node_end(list_t **, const char *, int);
 
 size_t print_list(const list_t *);
 list_t *node_starts_with(list_t *, char *, char);
-ssize_t get_node_index(list_t *, list_t *);+
+ssize_t get_node_index(list_t *, list_t *);
 size_t list_len(const list_t *);
 char **list_to_strings(list_t *);
 
